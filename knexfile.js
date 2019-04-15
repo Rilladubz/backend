009f0aa1c1ede99bd872a4e12196d57e500e2b3a
Kevin Smith <keveightysev@gmail.com>
2019-04-15 08:37:52 -0500

    Knex file configured

---
+++ knexfile.js
@@ -0,0 +1,48 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      user: 'postgres',
+      password: process.env.PG_PW,
+      database: 'productqueuedev'
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+
+  testing: {
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      user: 'postgres',
+      password: process.env.PG_PW,
+      database: 'productqueuetest'
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || {
+      database: 'productqueueprod',
+      user: 'postgres',
+      password: process.env.PG_PW
+    },
+    migrations: {
+      tableName: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  }
+}
