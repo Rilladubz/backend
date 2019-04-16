55b17240e550dd5703cc7aa70c766cc940d39505
Kevin Smith <keveightysev@gmail.com>
2019-04-16 10:58:33 -0500

    Start writing tests for model, updated response for endpoints to send more data than just the token

--- __tests__/users-model.js
+++ __tests__/users-model.js
@@ -3,13 +3,13 @@ const Users = require('../models/users.js')
 
 describe('Users model', () => {
   describe('newOrg()', () => {
-    it('should add the new organization', async () => {
+    it.skip('should add the new organization', async () => {
       await Users.newOrg({ name: 'Company' })
       const users = await db('organizations')
       expect(users).toHaveLength(3)
     })
 
-    it('should not add a new organization if it already exists', async () => {
+    it.skip('should not add a new organization if it already exists', async () => {
       await Users.newOrg({ name: 'Lambda School' })
       const users = await db('organizations')
       expect(users).toHaveLength(3)
@@ -20,9 +20,25 @@ describe('Users model', () => {
       expect(org).toBe(1)
     })
 
-    it('should return company id of new', async () => {
+    it.skip('should return company id of new', async () => {
       const [org] = await Users.newOrg({ name: 'Lambda School 2' })
       expect(org).toBe(4)
+    })
+  })
+
+  describe('newUser()', () => {
+    const testUser = {
+      first_name: 'Kevin',
+      last_name: 'Smith',
+      org_id: 1,
+      email: 'kevin@newuser.com',
+      password: 'password'
+    }
+
+    it('should add a new user', async () => {
+      await Users.newUser(testUser)
+      const users = db('users')
+      expect(users).toHaveLength(5)
     })
   })
 })
