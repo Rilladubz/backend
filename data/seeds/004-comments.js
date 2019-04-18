70a2fe206be67c2a1dd082eb9a5ac1dc7829e4b1
Kevin Smith <keveightysev@gmail.com>
2019-04-18 16:53:55 -0500

    Updated seeds

--- data/seeds/004-comments.js
+++ data/seeds/004-comments.js
@@ -1,14 +1,9 @@
 exports.seed = function(knex) {
   return knex('comments').insert([
     {
-      user_id: 4,
-      project_id: 2,
-      comment: 'Look at this comment'
-    },
-    {
-      user_id: 3,
+      user_id: 1,
       project_id: 1,
-      comment: "Don't look at this comment"
+      comment: 'Example comment'
     }
   ])
 }
